8dfc4fdaa63c997c3fff47b5c5d3052fea283194
mfhoss <mfhoss@yahoo.com>
2019-07-06 11:44:19 -0400

    Update AppTest.java

--- src/test/java/com/ej/AppTest.java
+++ src/test/java/com/ej/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple java app tets
+ * Unit test for simple java app tets and tets
  */
 public class AppTest 
     extends TestCase
